a178c2e3add48c95d7edc0522b4c8ee5b094a717
Nishant Sharma <nishant19072003@gmail.com>
2025-08-22 23:31:32 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         className={inter.className}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
